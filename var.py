25857bc6b7011a4681ae2e4d782615bf4a2d7394
HoJun Heo <116245224+HeoHoJun@users.noreply.github.com>
2023-12-28 21:06:38 +0900

    그릇의 달인 변수 만들기

---
+++ var.py
@@ -0,0 +1,9 @@
+# 변수 my_var에 원하는 자료를 넣어 보세요
+my_var1 = 900212     # 숫자 담기
+my_var2 = 'jinju'    # 문자열 담기
+my_var3 = [7, 'h', 'jinju']   # 리스트 담기
+
+# my_var를 출력해 보세요
+print(my_var1)
+print(my_var2)
+print(my_var3)
