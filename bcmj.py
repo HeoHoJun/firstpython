87a6911499ac3d5a36cc085b7dc7af9e305118a1
HoJun Heo <116245224+HeoHoJun@users.noreply.github.com>
2023-12-29 09:04:19 +0900

    빈칸 추론해 명제 만들기

--- bcmj.py
+++ bcmj.py
@@ -1,7 +1,9 @@
 # stat1이 True가 되도록 소괄호 대산에 명제 입력
+# stat1 = 2 < 4 and () 
 stat1 = 2 < 4 and 4 < 5
 
 # stat2가 False가 되도록 소괄호 대신에 명제 입력
+# stat2 = 4 >= 6 or ()
 stat2 = 4 >= 6 or 6 == 7
 
 # 두 변수를 출력해서 True, False를 출력
