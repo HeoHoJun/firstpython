66d868c5b55328280915bf206d74b372e311228c
HoJun Heo <116245224+HeoHoJun@users.noreply.github.com>
2023-12-31 09:14:51 +0900

    강화 무기 만들기

---
+++ kmm.py
@@ -0,0 +1,10 @@
+# 이순신 장군이 강화 묵기에 넣고 싶은 내용을 item1, item2, item3에 적였다.
+item1 = '완전 가볍고'
+item2 = '날카로우며'
+item3 = '손에 착착 감기는'
+
+# item1, item2, item3을 이어 붙여서 무기를 만들어 보자
+weapon = item1 + ' ' + item2 + ' ' + item3 + ' ' + '무기'
+
+# 만든 무기 weapon을 출력
+print(weapon)
