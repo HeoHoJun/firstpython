75879637c3fd42c77efaa50de928e744ad369d36
HoJun Heo <116245224+HeoHoJun@users.noreply.github.com>
2024-01-05 17:27:38 +0900

    튜플만들기

---
+++ dp.py
@@ -0,0 +1,16 @@
+# 1, 2, 3, 4, 5를 튜플 자료형으로 저장
+my_tuple = (1, 2, 3, 4, 5)
+
+# my_tuple을 인덱싱해 인덱스 2의 원소를 담아 보세요
+var1 = my_tuple[1]
+
+# my_tuple을 슬라이싱하여 원소 1, 2, 3을 담아 보세요
+var2 = my_tuple[0:3]
+
+# my_tuple의 길이를 담아 보세요
+var3 = len(my_tuple)
+
+# 출력
+print(var1)
+print(var2)
+print(var3)
