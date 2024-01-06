2ea1c2725446177f409e0ad3c6a72d93699c60da
HoJun Heo <116245224+HeoHoJun@users.noreply.github.com>
2024-01-06 12:18:05 +0900

    과일 사전 만들기

---
+++ dic.py
@@ -0,0 +1,11 @@
+# '사과'와 'apple', '바나나'와 'banana', '당근'과 'carrot'을 짝을 지어 넣어 주세요
+my_dict = {'사과':'apple', '바나나':'banana', '당근':'carrot'}
+
+# 사과를 영어로 뭐라고 하는지 my_dict에서 찾아 저장
+my_var = my_dict['사과']
+
+# 당근은 싫어요! my_dict에서 당근-carrot을 찾아 삭제
+del my_dict['당근']
+
+# 체리는 좋아요! my_dict에 체리-cherry를 추가
+my_dict['체리'] = 'cherry'
