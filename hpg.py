5051d6bd8cc9f5b584eef1b6d5b63dc3647051a9
HoJun Heo <116245224+HeoHoJun@users.noreply.github.com>
2024-01-07 09:38:14 +0900

    호박 고르기

---
+++ hpg.py
@@ -0,0 +1,12 @@
+# 호박 다섯 개가 튜플 자료형으로 들어 있다.
+pumpkin = (1, 5, 2, 3, 6)
+
+# 가장 큰 호박을 골라서 변수에 저장
+big = max(pumpkin)
+
+# 가장 작은 호박을 골라서 변수에 저장
+small = min(pumpkin)
+
+# 출력
+print(max(pumpkin))
+print(min(pumpkin))
