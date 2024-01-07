3fa82fbb4318af8750c271b7ac98b5759d105c02
HoJun Heo <116245224+HeoHoJun@users.noreply.github.com>
2024-01-07 09:43:43 +0900

    시퀀스의 합과 길이 구하기

---
+++ sqs.py
@@ -0,0 +1,16 @@
+# 숫자 다섯 개가 리스트에 담겨 있다.
+my_list = [1, 2, 3, 4, 5]
+
+# my_list의 합
+var1 = sum(my_list)
+
+# my_list의 길이
+var2 = len(my_list)
+
+# my_list 원소들의 평균
+var3 = sum(my_list) / len(my_list)
+
+# 출력
+print(var1)
+print(var2)
+print(var3)
